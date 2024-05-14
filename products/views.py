ea15a41356c846a36da583a7f3b39fbd214d99c3
Megan Walford <mwalford22@gmail.com>
2024-05-14 17:29:17 +0000

    Addition of products.html and setup

--- products/views.py
+++ products/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
 
 # Create your views here.
+def all_products(request):
+    """ All product views to display all plants and products """
+    
+    return render(request, 'products/products.html')
