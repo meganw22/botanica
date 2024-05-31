57895a0ffb7445dc341a41b797d0ca0f3c865bc7
Megan Walford <mwalford22@gmail.com>
2024-05-31 19:00:19 +0000

    Bag badge added to navbar

--- bag/contexts.py
+++ bag/contexts.py
@@ -3,6 +3,7 @@ from django.conf import settings
 def bag_contents(request):
     bag = request.session.get('bag', [])
     total_price = sum(float(item['price']) * item['quantity'] for item in bag)
+    total_quantity = sum(item['quantity'] for item in bag)
     free_delivery_threshold = settings.FREE_DELIVERY_THRESHOLD
     delivery_cost = 0 if total_price >= free_delivery_threshold else settings.STANDARD_DELIVERY_COST
     grand_total = total_price + delivery_cost
@@ -10,6 +11,7 @@ def bag_contents(request):
     context = {
         'bag': bag,
         'total_price': total_price,
+        'total_quantity': total_quantity,
         'free_delivery_threshold': free_delivery_threshold,
         'delivery_cost': delivery_cost,
         'grand_total': grand_total,
