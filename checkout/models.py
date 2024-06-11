95d0512d0856be1dd77fb39048bfe6194a277d9c
Megan Walford <mwalford22@gmail.com>
2024-06-11 18:19:59 +0000

    Added forms.py, admin.py and signals.py

--- checkout/models.py
+++ checkout/models.py
@@ -17,7 +17,7 @@ class Address(models.Model):
         verbose_name_plural = "Addresses"
 
     def __str__(self):
-        return f'{self.street_address1}, {self.city}, {self.state}, {self.country}'
+        return f'{self.street_address1}, {self.town_city}, {self.county}, {self.country}'
 
 class Order(models.Model):
     order_id = models.CharField(max_length=36, null=False, editable=False, unique=True)
@@ -26,20 +26,17 @@ class Order(models.Model):
     contact_number = models.CharField(max_length=20, null=False, blank=False)
     shipping_address = models.ForeignKey(Address, related_name='shipping_orders', on_delete=models.SET_NULL, null=True, blank=True)
     date_created = models.DateTimeField(auto_now_add=True)
-    delivery_fee = models.DecimalField(max_digits=10, decimal_places=2, default=0)
-    total_cost = models.DecimalField(max_digits=15, decimal_places=2, default=0)
-    final_amount = models.DecimalField(max_digits=15, decimal_places=2, default=0)
+    delivery_fee = models.DecimalField(max_digits=10, decimal_places=2, null=False, default=0)
+    total_cost = models.DecimalField(max_digits=15, decimal_places=2, null=False, default=0)
+    final_amount = models.DecimalField(max_digits=15, decimal_places=2, null=False, default=0)
 
     def _generate_order_id(self):
         """
         Generate a unique order ID using UUID.
         """
-        return str(uuid.uuid4())
+        return uuid.uuid4().hex.upper()
 
     def update_totals(self):
-        """
-        Update the total and final amounts, including delivery fee.
-        """
         self.total_cost = self.items.aggregate(Sum('item_total'))['item_total__sum'] or 0
         if self.total_cost < settings.FREE_DELIVERY_THRESHOLD:
             self.delivery_fee = self.total_cost * settings.STANDARD_DELIVERY_PERCENTAGE / 100
@@ -49,9 +46,6 @@ class Order(models.Model):
         self.save()
 
     def save(self, *args, **kwargs):
-        """
-        Override the original save method to set the order ID if it hasn't been set already.
-        """
         if not self.order_id:
             self.order_id = self._generate_order_id()
         super().save(*args, **kwargs)
@@ -60,18 +54,17 @@ class Order(models.Model):
         return f'Order {self.order_id}'
 
 class OrderItem(models.Model):
-    order = models.ForeignKey(Order, related_name='items', on_delete=models.CASCADE)
+    order = models.ForeignKey(Order, null=False, blank=False, related_name='items', on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, null=False, blank=False, on_delete=models.CASCADE)
     product_size = models.CharField(max_length=2, null=True, blank=False)
     quantity_ordered = models.PositiveIntegerField()
-    item_total = models.DecimalField(max_digits=10, decimal_places=2, editable=False)
+    item_total = models.DecimalField(max_digits=10, decimal_places=2, editable=False, null=True, blank=False)
 
     def save(self, *args, **kwargs):
-        """
-        Override the original save method to calculate the item total and update the order total.
-        """
-        self.item_total = self.product_size.plant.price * self.quantity_ordered
+        if self.product:
+            self.item_total = self.product.price * self.quantity_ordered
         super().save(*args, **kwargs)
         self.order.update_totals()
 
     def __str__(self):
-        return f'Item {self.product_size.plant.name} ({self.product_size.get_size_display()}) for order {self.order.order_id}'
+        return f'Item {self.product.easy_name} for order {self.order.order_id}'
