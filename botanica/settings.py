0cc52d1b57e10d68cd319801433beb75833453a4
Megan Walford <mwalford22@gmail.com>
2024-06-13 19:59:23 +0000

    Deployment of botanica app to heroku

--- botanica/settings.py
+++ botanica/settings.py
@@ -1,4 +1,5 @@
 import os
+import dj_database_url
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -14,7 +15,7 @@ SECRET_KEY = 'django-insecure-5!i*kt9!pvun3o@=81ymspylnr31uzoafvo57ops*!=vx0xqsa
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['8000-meganw22-botanica-9qn6y1vvq4a.ws-eu114.gitpod.io']
+ALLOWED_HOSTS = ['8000-meganw22-botanica-9qn6y1vvq4a.ws-eu114.gitpod.io', 'botanica-plants.herokuapp.com', 'localhost']
 
 
 # Application definition
@@ -107,12 +108,17 @@ WSGI_APPLICATION = 'botanica.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+if 'DATABASE_URL' in os.environ:
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        }
+    }
 
 
 # Password validation
