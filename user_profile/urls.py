c6611e1ad6a48b049709280ab05b0f962c17cada
Megan Walford <mwalford22@gmail.com>
2024-06-19 18:58:06 +0000

    Added better CRUD functionality to the user profile page

--- user_profile/urls.py
+++ user_profile/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path
 from . import views
+from .views import profile, edit_profile, delete_account, confirm_delete_account
 
 urlpatterns = [
     path('', views.profile, name='profile'),
     path('edit/', views.edit_profile, name='edit_profile'),
+    path('delete/', confirm_delete_account, name='confirm_delete_account'),
+    path('delete/confirm/', delete_account, name='delete_account'),
 ]
+
--- user_profile/views.py
+++ user_profile/views.py
@@ -2,34 +2,59 @@ from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User
 from checkout.models import Order
-from checkout.forms import AddressForm
 
+""" User Profile View """
 @login_required
 def profile(request):
-    orders = Order.objects.filter(email_address=request.user.email)
-    return render(request, 'user_profile/profile.html', {'orders': orders})
+    user = request.user
+    orders = Order.objects.filter(email_address=user.email)
+    latest_order = orders.order_by('-date_created').first() if orders.exists() else None
 
+    context = {
+        'user': user,
+        'orders': orders,
+        'latest_order': latest_order,
+    }
+
+    return render(request, 'user_profile/profile.html', context)
+
+"""Edit Profile"""
 @login_required
 def edit_profile(request):
     user = request.user
     latest_order = Order.objects.filter(email_address=user.email).order_by('-date_created').first()
 
     if request.method == 'POST':
-        address_form = AddressForm(request.POST, instance=latest_order)
-        if address_form.is_valid():
-            address_form.save()
-            
-            user.first_name = request.POST['first_name']
-            user.last_name = request.POST['last_name']
-            user.email = request.POST['email']
-            user.save()
-            
-            return redirect('profile')
-    else:
-        address_form = AddressForm(instance=latest_order)
+        # Update user details
+        user.first_name = request.POST.get('first_name', '')
+        user.last_name = request.POST.get('last_name', '')
+        user.email = request.POST.get('email', '')
+        user.save()
+
+        # Update latest order address details
+        if latest_order:
+            latest_order.address_line1 = request.POST.get('address_line1', '')
+            latest_order.address_line2 = request.POST.get('address_line2', '')
+            latest_order.city = request.POST.get('city', '')
+            latest_order.county = request.POST.get('county', '')
+            latest_order.postcode = request.POST.get('postcode', '')
+            latest_order.country = request.POST.get('country', '')
+            latest_order.save()
+
+        return redirect('profile')
 
     context = {
         'latest_order': latest_order,
-        'address_form': address_form,
     }
     return render(request, 'user_profile/editprofile.html', context)
+
+"""Delete Account"""
+@login_required
+def delete_account(request):
+    user = request.user
+    user.delete()
+    return redirect('home')
+
+@login_required
+def confirm_delete_account(request):
+    return render(request, 'user_profile/confirm_delete_account.html')
