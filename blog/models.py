70a0eff6ff55a1b6eb7d4640789a8cae3ad7e824
Megan Walford <mwalford22@gmail.com>
2024-06-27 22:53:52 +0000

    Plant blog app views and templates adjustments

--- blog/models.py
+++ blog/models.py
@@ -6,23 +6,25 @@ class Post(models.Model):
     Model to represent a blog post.
     """
     title = models.CharField(max_length=200)
-    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='blog_posts')
     content = models.TextField()
     published_date = models.DateTimeField(auto_now_add=True)
+    likes = models.ManyToManyField(User, related_name='liked_posts', blank=True)
 
     def __str__(self):
         return self.title
+
+    def total_likes(self):
+        return self.likes.count()
 
 class Comment(models.Model):
     """
     Model to represent a comment on a blog post.
     """
-    post = models.ForeignKey(
-        Post, on_delete=models.CASCADE, related_name='comments'
-    )
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='comments')
     author = models.ForeignKey(User, on_delete=models.CASCADE)
     content = models.TextField()
     published_date = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
-        return f'Comment by {self.author.username} on {self.post.title}'
+        return f'Comment by {self.author} on {self.post}'
