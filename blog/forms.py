a8beb714ddde40f8781d94132529fba86a7ba9e2
Megan Walford <mwalford22@gmail.com>
2024-06-29 22:12:37 +0000

    Filter Products max price validation added

--- blog/forms.py
+++ blog/forms.py
@@ -5,7 +5,10 @@ from .models import Post, Comment
 class PostForm(forms.ModelForm):
     class Meta:
         model = Post
-        fields = ['title', 'content']
+        fields = ['title', 'content', 'image']
+        widgets = {
+            'content': forms.Textarea(attrs={'rows': 5}),
+        }
 
 
 class CommentForm(forms.ModelForm):
