b5a42b26a8a0c55b1fab14159b7c7b920788fd21
Megan Walford <mwalford22@gmail.com>
2024-06-29 09:13:56 +0000

    Added validation to ensure empty search cannot be carried out

--- static/js/script.js
+++ static/js/script.js
@@ -1,7 +1,18 @@
+// ----------------------------------------- base.html
+
+// Search Bar Validation
+function validateSearchForm(form) {
+    var searchInput = form.querySelector('#searchInput').value.trim();
+    if (searchInput === '') {
+        alert('Please enter a search term.');
+        return false;
+    }
+    return true;
+}
+
 // -----------------------------------product_detail.html
 
 // Select height and show height price
-
 function selectHeight(height) {
     document.getElementById('selected-height-input').value = height;
     updatePriceDisplay(height);
@@ -26,6 +37,42 @@ function validateForm(event) {
     }
 }
 
+// Quantity Selectors
+function decreaseQuantity(button) {
+    var quantityInput = button.nextElementSibling;
+    var currentValue = parseInt(quantityInput.value);
+    if (currentValue > 1) {
+        quantityInput.value = currentValue - 1;
+    } else {
+        alert("Quantity cannot be less than 1.");
+    }
+    updateQuantity(quantityInput);
+}
+
+function increaseQuantity(button) {
+    var quantityInput = button.previousElementSibling;
+    var currentValue = parseInt(quantityInput.value);
+    if (currentValue < 99) {
+        quantityInput.value = currentValue + 1;
+    } else {
+        alert("Quantity cannot be more than 99.");
+    }
+    updateQuantity(quantityInput);
+}
+
+function updateQuantity(input) {
+    var value = parseInt(input.value);
+    if (isNaN(value) || value < 1) {
+        alert("Quantity must be a number between 1 and 99.");
+        input.value = 1;
+    } else if (value > 99) {
+        alert("Quantity must be a number between 1 and 99.");
+        input.value = 99;
+    }
+    document.getElementById('quantity-input').value = input.value;
+}
+
+// Show inital price value as smallest price available
 document.addEventListener("DOMContentLoaded", function () {
     updateFormInputs();
     var initialHeight = document.getElementById('selected-height-input').value || 'sm';
